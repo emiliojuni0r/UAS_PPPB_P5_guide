02a3da1b89ac3f018df824758c903781236d270b
emiliojuni0r <emiliohamsyahjr@gmail.com>
2024-12-07 21:15:16 +0700

    Initial commit (layout only)

---
+++ app/src/main/java/com/example/uas_pppb_p5_guide/Admin/AdminViewActivity.kt
@@ -0,0 +1,21 @@
+package com.example.uas_pppb_p5_guide.Admin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.uas_pppb_p5_guide.R
+
+class AdminViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_admin_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
